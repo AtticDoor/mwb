7ae90a734de5d45e3c7d21ffd86b06a788f3d48c
AtticDoor <the13thdoll@gmail.com>
2023-09-28 16:21:58 -0500

    Code Cleanup

--- Assets/Scripts/LerpObject.cs
+++ Assets/Scripts/LerpObject.cs
@@ -3,21 +3,32 @@ using System.Collections;
 
 [System.Serializable]
 public partial class LerpObject : MonoBehaviour
-{
-    public static bool motion;
-    public static IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+{   
+    public static IEnumerator MoveEnemy(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
     {
-        EnemyScript PO = (EnemyScript) thisTransform.GetComponent("EnemyScript");
-        LerpObject.motion = true;
+        EnemyScript PO = (EnemyScript)thisTransform.GetComponent("EnemyScript");
         float i = 0f;
         float rate = 1f / time;
         while (i < 1f)
         {
             if ((PO != null) && PO.On)
             {
-                i = i + (Time.deltaTime * rate);
+                i += (Time.deltaTime * rate);
                 thisTransform.position = Vector3.Lerp(startPos, endPos, i);
             }
+            yield return null;
+        }
+    }
+
+    public static IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        float i = 0f;
+        float rate = 1f / time;
+        while (i < 1f)
+        {
+            i += (Time.deltaTime * rate);
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            
             yield return null;
         }
     }
@@ -28,7 +39,7 @@ public partial class LerpObject : MonoBehaviour
         float rate = 1f / time;
         while (i < 1f)
         {
-            i = i + (Time.deltaTime * rate);
+            i += (Time.deltaTime * rate);
             thisTransform.localScale = Vector3.Lerp(startPos, endPos, i);
             yield return null;
         }
@@ -40,10 +51,9 @@ public partial class LerpObject : MonoBehaviour
         float rate = 1f / time;
         while (i < 1f)
         {
-            i = i + (Time.deltaTime * rate);
+            i += (Time.deltaTime * rate);
             thisTransform.localEulerAngles = Vector3.Lerp(startPos, endPos, i);
             yield return null;
         }
     }
-
 }
--- Assets/Scripts/mapCameraScript.cs
+++ Assets/Scripts/mapCameraScript.cs
@@ -2,46 +2,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [System.Serializable]
-public partial class mapCameraScript : MonoBehaviour
+public partial class MapCameraScript : MonoBehaviour
 {
-    public int phase;
-    public GameObject src;
-    public GameObject dest;
-    public float startTime;
+    private float startTime;
     public virtual void Start()
     {
         startTime = Time.time;
-        Debug.Log((MainScript.curLevel + " ") + MainScript.lastLevel);
-        phase = 1;
-        src = GameObject.Find(("Scene" + MainScript.lastLevel) + MainScript.curLevel);
-        dest = GameObject.Find((MainScript.curLevel + "Scene") + MainScript.lastLevel);
         Camera.main.orthographicSize = 0.07f;
-        Vector3 srcPos = src.transform.position;
-        srcPos.z = Camera.main.transform.position.z;
-        Vector3 destPos = dest.transform.position;
+
+        Vector3 srcPos = GameObject.Find("Scene" + MainScript.lastLevel + MainScript.curLevel).transform.position;
+        Vector3 destPos = GameObject.Find(MainScript.curLevel + "Scene" + MainScript.lastLevel).transform.position;
+        srcPos.z = Camera.main.transform.position.z; 
         destPos.z = Camera.main.transform.position.z;
+
+        //move camera - zoom in and out happens separately in Update
         StartCoroutine(LerpObject.MoveObject(Camera.main.transform, srcPos, destPos, 5));
     }
 
     public virtual void Update()
     {
+        //zoom in and out - motion from src to dest is in Start()
         if ((Time.time - startTime) < 2.5f)
-        {
             Camera.main.orthographicSize = Camera.main.orthographicSize + (Time.deltaTime * 2);
-            if (Camera.main.orthographicSize > 6)
-            {
-                phase = 3;
-            }
-        }
         else
         {
-            // if (phase==3)
             Camera.main.orthographicSize = Camera.main.orthographicSize - (Time.deltaTime * 2);
-            if (Camera.main.orthographicSize < 0.07f)
-            {
-                SceneManager.LoadScene(MainScript.curLevel);
-            }
+            if (Camera.main.orthographicSize < 0.07f)            
+                SceneManager.LoadScene(MainScript.curLevel);            
         }
     }
-
 }
